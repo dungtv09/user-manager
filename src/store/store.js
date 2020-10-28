d8c117d02bea1759b6e99cca02bc97dc9fa4bcb5
Viet Dung <dungtv09@gmail.com>
2020-10-28 16:31:40 +0700

    change promise to async/await

--- src/store/store.js
+++ src/store/store.js
@@ -19,34 +19,29 @@ export default new Vuex.Store({
     },
 
     fetchData({ commit, state }) {
-      let pageNumber = -1;
-      let pages = -1;
-      let usersTemp = [];
-      let getPageNumber = new Promise(function(resolve, reject) {
-        fetch('https://reqres.in/api/users')
-          .then(response => response.json())
-          .then(data => {
-            pages = data.total_pages;
-            resolve(pages);
-          });
-      });
-      getPageNumber.then(pages => {
-        pageNumber = pages;
-        for (let i = 1; i <= pageNumber; i++) {
-          let getUsers = new Promise(function(resolve, reject) {
-            fetch('https://reqres.in/api/users?page=' + i)
-              .then(response => response.json())
-              .then(data => {
-                usersTemp = data.data;
-                resolve(usersTemp);
-              });
-          });
-          getUsers.then(usersTemp => {
-            state.users = state.users.concat(usersTemp);
-            usersTemp = [];
-          });
+      async function getPageNumber() {
+        let res = await fetch('https://reqres.in/api/users');
+        let data = await res.json();
+        let pages = data.total_pages;
+        return pages;
+      }
+
+      async function getUsers(pages) {
+        let users = [];
+        for (let i = 1; i <= pages; i++) {
+          let res = await fetch('https://reqres.in/api/users?page=' + i);
+          let data = await res.json();
+          let usersTemp = data.data;
+          users = users.concat(usersTemp);
         }
-      });
+        return users;
+      }
+
+      getPageNumber()
+        .then(pages => getUsers(pages))
+        .then(users => {
+          state.users = users;
+        });
     },
 
     refreshUserList({ commit, state }) {
@@ -67,40 +62,40 @@ export default new Vuex.Store({
       ) {
         alert('Input field must be filled out');
       } else {
-        fetch('https://reqres.in/api/users', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(payload)
-        })
-          .then(response => response.json())
-          .then(data => {
-            alert(
-              'New user "' +
-                payload.firstName +
-                ' ' +
-                payload.lastName +
-                '" with id ' +
-                data.id +
-                ' created'
-            );
+        async function submit() {
+          let res = await fetch('https://reqres.in/api/users', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
           });
+          let data = await res.json();
+          alert(
+            'New user "' +
+              payload.firstName +
+              ' ' +
+              payload.lastName +
+              '" with id ' +
+              data.id +
+              ' created'
+          );
+        }
+        submit();
       }
     }, // Xóa user
-    removeUser({ commit }, payload) {
-      fetch('https://reqres.in/api/users/' + payload[0].index, {
+    async removeUser({ commit }, payload) {
+      let res = await fetch('https://reqres.in/api/users/' + payload[0].index, {
         method: 'DELETE'
-      }).then(response => {
-        alert(
-          'Removed user: ' +
-            payload[0].user.first_name +
-            ' ' +
-            payload[0].user.last_name
-        );
-        //refresh lại user list
-        payload[1].push('/user').catch(err => {});
       });
+      alert(
+        'Removed user: ' +
+          payload[0].user.first_name +
+          ' ' +
+          payload[0].user.last_name
+      );
+      //refresh lại user list
+      payload[1].push('/user').catch(err => {});
     },
 
     editUser({ commit }, payload) {
@@ -136,47 +131,45 @@ export default new Vuex.Store({
     },
 
     //sign in
-    signIn({ commit }, payload) {
-      fetch('https://reqres.in/api/login', {
+    async signIn({ commit }, payload) {
+      let res = await fetch('https://reqres.in/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(payload[0])
-      }).then(function(res) {
-        if (res.status !== 200) {
-          res.json().then(data => {
-            alert('Error: ' + data.error);
-          });
-          return;
-        }
-        res.json().then(data => {
-          localStorage.setItem('isSignIn', true);
-          payload[1].push('/user');
-        });
       });
+      let status = await res.status;
+      let data = await res.json();
+      let err = await data.error;
+
+      if (status !== 200) {
+        alert('Error: ' + err);
+      } else {
+        localStorage.setItem('isSignIn', true);
+        payload[1].push('/user');
+      }
     },
 
     //sign in
-    signUp({ commit }, payload) {
-      fetch('https://reqres.in/api/register', {
+    async signUp({ commit }, payload) {
+      let res = await fetch('https://reqres.in/api/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(payload[0])
-      }).then(function(res) {
-        if (res.status !== 200) {
-          res.json().then(data => {
-            alert('Error: ' + data.error);
-          });
-          return;
-        }
-        res.json().then(data => {
-          alert('Sign up success. Please sign in');
-          payload[1].push('/signin');
-        });
       });
+      let status = await res.status;
+      let data = await res.json();
+      let err = await data.error;
+
+      if (status !== 200) {
+        alert('Error: ' + err);
+      } else {
+        alert('Sign up success. Please sign in');
+        payload[1].push('/signin');
+      }
     },
 
     //sign out
