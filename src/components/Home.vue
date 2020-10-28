0bac3996630fab2064b8475e275320cd1545eab7
Viet Dung <dungtv09@gmail.com>
2020-10-28 12:17:01 +0700

    add scoped css

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="home container text-center mt-5">
-    <h1 class="align-middle">
-      <img src="@/assets/logo.jpg" alt="logo" width="100px" />
-      Introduce
-    </h1>
-    <p class="mt-5">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet veritatis
-      eveniet officia quasi molestias et vitae cupiditate dolorum commodi odio
-      in, ab, natus numquam suscipit accusantium provident doloribus pariatur
-      expedita? Tenetur iste sit ad soluta iusto numquam nulla, aliquam autem
-      sequi modi dicta, quos reprehenderit ipsam magni veniam? Iste quo
-      exercitationem consectetur, expedita commodi, incidunt corporis iure eos
-      debitis voluptate placeat ex numquam recusandae dolorum nihil, veniam
-      labore id? Dolores ab fuga libero quasi vel. In voluptatibus odit autem
-      ipsum officiis id molestiae similique natus, ea soluta corporis facere
-      debitis sint eaque sapiente dolorem sequi quisquam. Mollitia, natus.
-      Autem, reprehenderit.
-    </p>
+  <div class="mt-5">
+    <div class="home container text-center">
+      <h1 class="align-middle">
+        <img src="@/assets/logo.jpg" alt="logo" width="100px" />
+        Introduce
+      </h1>
+      <p class="mt-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet veritatis
+        eveniet officia quasi molestias et vitae cupiditate dolorum commodi odio
+        in, ab, natus numquam suscipit accusantium provident doloribus pariatur
+        expedita? Tenetur iste sit ad soluta iusto numquam nulla, aliquam autem
+        sequi modi dicta, quos reprehenderit ipsam magni veniam? Iste quo
+        exercitationem consectetur, expedita commodi, incidunt corporis iure eos
+        debitis voluptate placeat ex numquam recusandae dolorum nihil, veniam
+        labore id? Dolores ab fuga libero quasi vel. In voluptatibus odit autem
+        ipsum officiis id molestiae similique natus, ea soluta corporis facere
+        debitis sint eaque sapiente dolorem sequi quisquam. Mollitia, natus.
+        Autem, reprehenderit.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -24,7 +26,7 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
 .home p {
   line-height: 2rem;
 }
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center">
-    <form class="form-signin mt-5">
+    <form class="form-sign mt-5">
       <img
         class="mb-4"
         src="../assets/logo.jpg"
@@ -64,34 +64,4 @@ export default {
 };
 </script>
 
-<style>
-.form-signin {
-  width: 100%;
-  max-width: 330px;
-  padding: 15px;
-  margin: auto;
-}
-.form-signin .checkbox {
-  font-weight: 400;
-}
-.form-signin .form-control {
-  position: relative;
-  box-sizing: border-box;
-  height: auto;
-  padding: 10px;
-  font-size: 16px;
-}
-.form-signin .form-control:focus {
-  z-index: 2;
-}
-.form-signin input[type='email'] {
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
-}
-.form-signin input[type='password'] {
-  margin-bottom: 10px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-}
-</style>
+<style></style>
